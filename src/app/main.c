f7b3b0b3d939bb06956789f77e406022a3085c48
Matthias Nigmann <7ta5j2nq@duck.com>
2025-08-29 18:08:28 -0400

    Working on new member board

--- src/app/main.c
+++ src/app/main.c
@@ -28,7 +28,7 @@ int main(void) {
     HAL_Init();
 
     // Drivers
-    core_heartbeat_init(GPIOA, GPIO_PIN_5);
+    core_heartbeat_init(GPIOA, GPIO_PIN_8);
     core_GPIO_set_heartbeat(GPIO_PIN_RESET);
 
     if (!core_clock_init()) error_handler();

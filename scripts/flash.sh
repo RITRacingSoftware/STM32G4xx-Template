8c484144a07e19117b251a7b520db05de65222c3
Matthias Nigmann <7ta5j2nq@duck.com>
2025-09-06 19:41:34 -0400

    Fix flash script

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -10,14 +10,26 @@ if [[ $(cat Makefile) =~ PROJECT_VERSION[[:space:]]*:=[[:space:]]*([^[:space:]]*
     PROJECT_VERSION=${BASH_REMATCH[1]}
 fi
 
-ELF=build/stm32/$PROJECT_NAME-$PROJECT_VERSION.elf
+FNAME=$PWD/build/stm32/$PROJECT_NAME-$PROJECT_VERSION
 
-echo "Programming with $ELF"
+echo "Programming with $FNAME"
 
 mkfifo .uploader_telnet_fifo
-echo -e "program ${ELF} verify reset\nexit" > /tmp/uploader_telnet_fifo &
-tail -f /tmp/uploader_telnet_fifo | telnet localhost 4444
-if [ $? != 0 ]; then
-    openocd -f ./openocd.cfg -c "program ${ELF} verify reset" -c "exit"
+
+echo -e "program ${FNAME}.elf verify reset\nexit" > .uploader_telnet_fifo &
+telnet localhost 4444 < .uploader_telnet_fifo | grep -q "Connected to"
+if [ $? == 0 ]; then 
+    echo "Successfully uploaded with openocd"
+    rm .uploader_telnet_fifo;
+    exit 0; 
 fi
+
+echo -e "program -1 ${FNAME}.ihex\nexit" > .uploader_telnet_fifo &
+telnet localhost 4445 < .uploader_telnet_fifo | grep -q "Connected to"
+if [ $? == 0 ]; then 
+    rm .uploader_telnet_fifo
+    exit 0; 
+fi
+
+openocd -f ./openocd.cfg -c "program ${FNAME}.elf verify reset" -c "exit"
 rm .uploader_telnet_fifo
